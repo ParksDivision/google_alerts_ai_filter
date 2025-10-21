a6ea36eea1ef3f25c9b2fc7bcc9e1a5463c03272
Kyle Parks <kyleparks@Candaces-MacBook-Air.local>
2025-10-21 14:42:51 -0500

    getting rid of dist files for cleanup

--- dist/analysis/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { getCostInformation } from './claudeClient.js';
-import { AnalyzedArticle } from './relevanceAnalyzer.js';
-import { ArticleOutput } from '../scraper/index.js';
-/**
- * Analyzes content based on provided criteria and returns sorted results
- */
-export declare function analyzeContent(articles: ArticleOutput[], criteria: string): Promise<AnalyzedArticle[]>;
-export { AnalyzedArticle, getCostInformation };
